b1b9bcca711a3a06d7d5b36a8ea5d9532aacc159
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2025-07-14 18:18:11 +0200

    prove false using 4.11

--- ArkLib/ProofSystem/Whir/GenMutualCorrAgreement.lean
+++ ArkLib/ProofSystem/Whir/GenMutualCorrAgreement.lean
@@ -9,6 +9,7 @@ import ArkLib.Data.CodingTheory.InterleavedCode
 import ArkLib.Data.CodingTheory.ReedSolomon
 import ArkLib.Data.Probability.Notation
 import ArkLib.ProofSystem.Whir.ProximityGen
+import Mathlib.Algebra.Field.ZMod
 
 namespace CorrelatedAgreement
 
@@ -112,7 +113,7 @@ example : False := absurd (genMutualCorrAgreement_le_bound (ι := ι))
 
   function `Gen(parℓ,α)={1,α,..,α ^ parℓ-1}`
 -/
-noncomputable def gen_α (α : F) (parℓ : Type) (exp : parℓ → ℕ): F → parℓ → F :=
+noncomputable def gen_α (α : F) (parℓ : Type) (exp : parℓ → ℕ) : F → parℓ → F :=
   fun _ j => α ^ (exp j)
 
 /-- The proximity generator for smooth ReedSolomon codes wrt function
@@ -153,8 +154,112 @@ lemma genMutualCorrAgreement_rsc_le_bound
     BStar = (1 - rate) / 2 ∧
     errStar = fun _ => ENNReal.ofReal
         ((Fintype.card Gen.parℓ - 1) * (2^m / rate * (Fintype.card F)))
-  := by sorry
+  := by 
+  sorry
 
+def FALSEPROP2 : Prop := ∀ [DecidableEq ι] {Gen Gen_α: ProximityGenerator ι F}
+  [Fintype Gen.parℓ] [Fintype Gen_α.parℓ]
+  {α : F} {φ : ι ↪ F} {m : ℕ} [Smooth φ] {exp : Gen.parℓ → ℕ}
+  {BStar rate : ℝ} {errStar : ℝ → ENNReal}
+  -- `Gen_α` is the proximity generator wrt function `Gen{parℓ, α}`
+  {hGen : Gen_α = proximityGenerator_α Gen α φ m exp}
+  -- the proof that `Gen_α` is the proximity generator with mutual correlated agreement
+  -- with proximity bound parameters BStar and errStar
+  {h : genMutualCorrAgreement Gen_α BStar errStar}
+  {hrate : rate = LinearCode.rate (smoothCode φ m)},
+    BStar = (1 - rate) / 2 ∧
+    errStar = fun _ => ENNReal.ofReal
+        ((Fintype.card Gen.parℓ - 1) * (2^m / rate * (Fintype.card F)))
+
+lemma genMutualCorrAgreement_rsc_le_bound_NOOOPE :
+  ¬FALSEPROP2 (F := ZMod 2) (ι := Unit) := by
+  unfold FALSEPROP2
+  simp only [ZMod.card, Nat.cast_ofNat, not_forall, Classical.not_imp, not_and, exists_and_left,
+    exists_and_right]
+  use inferInstance
+  let Gen : ProximityGenerator Unit (ZMod 2) := {
+    C := 0
+    parℓ := Fin 1
+    hℓ := inferInstance
+    Fun := fun _ _ ↦ 42
+    B := fun _ _ ↦ 1
+    err := fun _ _ _ ↦ 1
+    proximity := by
+      introv h₁ h₂
+      simp at h₁
+      rcases δ
+      simp at h₁
+      linarith
+  }
+  use Gen
+  let φ : Unit ↪ ZMod 2 := ⟨fun _ ↦ 1, by unfold Function.Injective; grind⟩
+  have : Smooth φ := by
+    constructor
+    swap; use 0; rfl
+    case a =>
+      constructor
+      case val => exact 1
+      case inv => exact 1
+      rfl
+      rfl
+      done
+    case H =>
+      constructor
+      swap
+      constructor
+      swap
+      constructor
+      swap
+      exact {⟨1, 1, rfl, rfl⟩}
+      simp
+      rfl
+      simp
+      rfl
+      simp
+    simp [Set.image]
+    simp [φ]
+    done
+  set Genα := @proximityGenerator_α _ _ _ _ _ _ _ _ Gen _ 0 φ 0 inferInstance fun _ ↦ 0 with EQ
+  use Genα
+  use inferInstance
+  let Y : Fintype Genα.parℓ := by simp [Genα, proximityGenerator_α]; infer_instance
+  use Y
+  use 1
+  use φ
+  use 0
+  use inferInstance
+  use fun _ ↦ 0
+  use (1 - ↑(ρ smoothCode φ 0)) / 2 + 1
+  
+  use LinearCode.rate (smoothCode φ 0)
+  use fun _ ↦ 0
+  simp only [Fintype.card_unique, Nat.cast_one, sub_self, pow_zero, one_div, zero_mul,
+    ENNReal.ofReal_zero, not_true_eq_false, imp_false, exists_const, genMutualCorrAgreement,
+    proximityCondition, mul_one, gt_iff_lt, ne_eq, bind_pure_comp, nonpos_iff_eq_zero, exists_prop,
+    proximityGenerator_α, exists_and_left]
+  
+  simp only [smoothCode, pow_zero]
+  simp only [LinearCode.rate, LinearCode.dim, Module.finrank, Module.rank, LinearCode.length,
+    Fintype.card_unique, Nat.cast_one, div_one, NNRat.cast_natCast, add_eq_left, one_ne_zero,
+    not_false_eq_true, and_true]
+  simp only [proximityGenerator_α, Submodule.zero_eq_bot, ProximityGenerator.mk.injEq, heq_eq_eq,
+    and_self, and_true, true_and, sub_self, Finset.univ_unique, Fin.default_eq_zero, Fin.isValue,
+    Finset.sum_singleton, isEmpty_Prop, not_lt, zero_le_coe, IsEmpty.forall_iff, implies_true, Genα,
+    Gen]
+  simp only [smoothCode, pow_zero, true_and, φ]
+  ext x y
+  simp [gen_α]
+
+example : False := absurd (genMutualCorrAgreement_rsc_le_bound (F := ZMod 2) (ι := Unit))
+                          (by have := genMutualCorrAgreement_rsc_le_bound_NOOOPE
+                              unfold FALSEPROP2 at this
+                              simp only [ZMod.card, Nat.cast_ofNat, not_forall, Classical.not_imp,
+                                not_and, exists_and_left, exists_and_right] at this
+                              simp only [ZMod.card, Nat.cast_ofNat, not_forall, Classical.not_imp,
+                                not_and, exists_and_left, exists_and_right]
+                              rcases this with ⟨_, this⟩
+                              exact this
+                          )
 
 /-- Conjecture 4.12
   The function `Gen(parℓ,α)={1,α,..,α ^ parℓ-1}` is a proximity generator with
