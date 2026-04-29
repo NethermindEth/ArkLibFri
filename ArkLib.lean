c62b4a31c1600f3c30c59e8d427b521a98dcfa4a
Ilia Vlasov <elijah.vlasov@gmail.com>
2026-04-29 11:55:04 +0100

    Add ExtEval

--- ArkLib.lean
+++ ArkLib.lean
@@ -175,3 +175,4 @@ import ArkLib.ProofSystem.Whir.RBRSoundness
 import ArkLib.ToMathlib.BigOperators.Fin
 import ArkLib.ToMathlib.Finset.Basic
 import ArkLib.ToMathlib.List.Basic
+import ArkLib.ToMathlib.Polynomial.ExtEval
